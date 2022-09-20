8b0c8827b08f4ecdf1acf2422fca81ce2276e9e1
bap <bap@MacBook-Pro-cua-Mac.local>
2022-09-20 10:48:11 +0700

    update playlist.py

--- playlist.py
+++ playlist.py
@@ -1,3 +1,4 @@
+from genericpath import exists
 from telnetlib import VT3270REGIME
 from io import BytesIO
 import openpyxl
@@ -8,6 +9,8 @@ import isodate
 import time
 import os
 import gspread
+import urllib.parse as urlparse
+
 
 #Read the spreadsheet
 spreadsheetId = "1glco8PeP_JmvgcEdDmDqeqHXVXQhCyqV53Dn5RPPhvE"
@@ -18,31 +21,66 @@ xlsx = openpyxl.load_workbook(filename=data)
 List = pd.read_excel(data, sheet_name='List music')
 History = pd.read_excel(data, sheet_name='History')
 
-#put data on sheet to array
-linkytb = List.Music.to_string(index=False).split()
-
-#get the time of youtube's link
-video_id1 = linkytb[0][-11:]
-video_id2 = linkytb[1][-11:]
 API_KEY = 'AIzaSyBj3flosOnOYncUolreFO5aeTMNfeurdzQ'
-respone1 = requests.get(f'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id={video_id1}&key={API_KEY}')
-respone2 = requests.get(f'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id={video_id2}&key={API_KEY}')
-data1 = respone1.json()
-data2 = respone2.json()
-toki1 = data1['items'][0]['contentDetails']['duration'] #case link lỗi, case data trống,
-toki2 = data2['items'][0]['contentDetails']['duration']
-duration1 = isodate.parse_duration(toki1).total_seconds()
-duration2 = isodate.parse_duration(toki2).total_seconds()
+
+def getDuration (video_id):
+    respone = requests.get(f'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id={video_id}&key={API_KEY}')
+    data = respone.json()
+    if not data.get('items'):
+        return -1
+    else:
+        toki = data['items'][0]['contentDetails']['duration']
+        duration = isodate.parse_duration(toki).total_seconds()
+        return duration
+
+#check and claim right data and put data of two song into array
+def getTwoSongAvailable (List):
+    linkytb = List.Music.to_string(index=False).split()
+    id_and_duration1 = []
+    id_and_duration2 = []
+    two_song_info = []
+    for temp in range (len(linkytb)):
+        if ("watch?v=" in linkytb[temp]) is True:
+            url_data = urlparse.urlparse(linkytb[temp])
+            query = urlparse.parse_qs(url_data.query)
+            video_id = query["v"][0]
+            if getDuration(video_id) == -1:
+                temp += 1
+            else:
+                id_and_duration1.append(video_id)
+                id_and_duration1.append(getDuration(video_id))
+                two_song_info.append(id_and_duration1)
+                if len(two_song_info) == 2:
+                    break
+                else: temp += 1
+            # continue
+        elif ("youtu.be" in linkytb[temp]) is True:
+            video_id = linkytb[temp][-11:]
+            if getDuration(video_id) == -1:
+                temp += 1
+            else:
+                id_and_duration2.append(video_id)
+                id_and_duration2.append(getDuration(video_id))
+                two_song_info.append(id_and_duration2)
+                if len(two_song_info) == 2:
+                    break
+                else: temp += 1
+                # continue
+        else:
+            temp += 1
+            # continue
+    return (two_song_info)
+print(getTwoSongAvailable(List))
 
 #run 2 top of list
-webbrowser.open(linkytb[0])
-time.sleep(duration1 + 7)
-os.system("killall -9 'Google Chrome'")
-webbrowser.open(linkytb[1])
-time.sleep(duration2 + 13)
-os.system("killall -9 'Google Chrome'")
+# webbrowser.open(linkytb[0])
+# time.sleep(duration1 + 7)
+# os.system("killall -9 'Google Chrome'")
+# webbrowser.open(linkytb[1])
+# time.sleep(duration2 + 13)
+# os.system("killall -9 'Google Chrome'")
 
-#move 2 top of list to [History] after play music
+# #move 2 top of list to [History] after play music
 # client = gspread.service_account(filename="cool-plasma-362004-5043b3ab9bf9.json")
 # sh = client.open("List music")
 # wkslist = sh.worksheet("List")

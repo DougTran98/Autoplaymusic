fd174c5bbcdb82e3e995ac34b9a8650633c775c9
bap <bap@MacBook-Pro-cua-Mac.local>
2022-09-23 18:20:08 +0700

    test.py final 1

--- playlist.py
+++ playlist.py
@@ -1,5 +1,6 @@
 #import
 from genericpath import exists
+from itertools import count
 from telnetlib import VT3270REGIME
 from io import BytesIO
 from tempfile import tempdir
@@ -35,9 +36,9 @@ def getDuration(video_id):
         duration = isodate.parse_duration(toki).total_seconds()
     return duration
 
-#is youtube url ?
-def isYoutubeLink(link_in_sheet):
-    if (("watch?v=" in link_in_sheet) or ("youtu.be" in link_in_sheet)):
+#is youtube url
+def isYoutubeLink(link_youtube):
+    if (("watch?v=" in link_youtube) or ("youtu.be" in link_youtube) or (("youtu.be" in link_youtube) and ("?t=" in link_youtube))):
         return True
     else:
         return False
@@ -49,67 +50,101 @@ def classifyYoutubeLink(link_youtube):
     else:
         return 1
 
-#check remain available link. Is youtube link still can play?
-def countAvailableLink(video_id, listSong):
-    client = gspread.service_account(filename="cool-plasma-362004-5043b3ab9bf9.json")
-    sh = client.open("List music") # This is file's name not a sheet name
-    wkslist = sh.worksheet("Copy List music")
-    linkytb = listSong.Music.to_string(index=False).split()
-    count = 0
-    for i in range(len(linkytb)):
-        if  isYoutubeLink(linkytb[i]) is True:
-            if getDuration(video_id) == -1:
-                count = count
-            else:
-                count += 1
-        else:
-            wkslist.delete_rows(i+1)
-    return count
-
-#check if need backup music or not
-def needBackUp(video_id, listSong):
-    if countAvailableLink(video_id, listSong) == 0:
-        return 2
-    if countAvailableLink(video_id, listSong) == 1:
-        return 1 
-    if countAvailableLink(video_id, listSong) >= 2:
-        return 0
-
 #get ID of all available link
 def getIdFromLink(link_youtube):
-    # linkytb = listSong.Music.to_string(index=False).split()
     if classifyYoutubeLink(link_youtube) == 1:
+        if ("?t=" not in link_youtube):
+            video_id = link_youtube[-11:]
+        else:
+            video_id = (link_youtube.split('/')[-1]).split('?')[0]
+    else:
         url_data = urlparse.urlparse(link_youtube)
         query = urlparse.parse_qs(url_data.query)
         video_id = query["v"][0]
-    else:
-        video_id = link_youtube[-11:]
     return video_id
 
-#add 2 music to array (music can be from list or backup)
-def getTwoSongAvailable(listSong):
+#check remain available link. Is youtube link still can play? If not available clear from list
+def countAvailableLink(listSong):
+    client = gspread.service_account(filename="cool-plasma-362004-5043b3ab9bf9.json")
+    sh = client.open("List music") # This is file's name not a sheet name
+    wkslist = sh.worksheet("test")
     linkytb = listSong.Music.to_string(index=False).split()
+    count = 0
+    for i in range (len(linkytb)-1, -1, -1):
+        if  isYoutubeLink(linkytb[i]) is True:
+            if getDuration(getIdFromLink(linkytb[i])) == -1:
+                count = count
+                wkslist.delete_rows(i+2)
+            else:
+                count += 1
+        else:
+            wkslist.delete_rows(i+2)
+    new_Sheet = readSheet()[0]
+    return count, new_Sheet
+
+# check if need backup music or not
+def needBackUp():
+    if countAvailableLink(readSheet()[0])[0] == 0:
+        return 2
+    if countAvailableLink(readSheet()[0])[0] == 1:
+        return 1 
+    if ((countAvailableLink(readSheet()[0])[0] == 2) or (countAvailableLink(readSheet()[0])[0] >2)):
+        return 0
+
+#add 2 music to array (music can be from list or backup)
+def getTwoSongAvailable():
+    new_Sheet = countAvailableLink(readSheet()[0])[1]
+    Backup = readSheet()[1]
+    linkytb = new_Sheet.Music.to_string(index=False).split()
+    linkbackup = Backup.Music.to_string(index=False).split()
     two_song_info = []
-    if needBackUp == 2:
-        two_song_info.append(getIdFromLink(readSheet()[1][0]))#them 2 bai tu backup list vao mang
-        two_song_info.append(getIdFromLink(readSheet()[1][1]))
-    if needBackUp == 1:
-        two_song_info.append(getIdFromLink(linkytb[0][0])) #them bai duy nhat trong list vao mang
-        two_song_info.append(getIdFromLink(readSheet()[1][0])) #them 1 bai tu backup list vao mang
-    if needBackUp == 0:
-        two_song_info.append(getIdFromLink(linkytb[0][0])) #them 2 bai tu trong list vao mang
-        two_song_info.append(getIdFromLink(linkytb[0][1]))
+    if needBackUp() == 2:
+        two_song_info.append(getIdFromLink(linkbackup[0]))#them 2 bai tu backup list vao mang
+        two_song_info.append(getIdFromLink(linkbackup[1]))
+    elif needBackUp() == 1:
+        two_song_info.append(getIdFromLink(linkytb[0])) #them bai duy nhat trong list vao mang
+        two_song_info.append(getIdFromLink(linkbackup[0])) #them 1 bai tu backup list vao mang
+    elif needBackUp() == 0:
+        two_song_info.append(getIdFromLink(linkytb[0])) #them 2 bai tu trong list vao mang
+        two_song_info.append(getIdFromLink(linkytb[1]))
     return two_song_info
 
 # run 2 song in array
 def runTwoSong():
-    two_song_in_list = getTwoSongAvailable(readSheet()[0])
+    two_song_in_list = getTwoSongAvailable()
     for i in range (len(two_song_in_list)):
         webbrowser.open(f"https://www.youtube.com/watch?v={two_song_in_list[i]}")
-        time.sleep(getDuration(getIdFromLink(two_song_in_list[i])) + 9)
+        time.sleep(getDuration(two_song_in_list[i]) + 9)
         os.system("killall -9 'Google Chrome'")
     return 
-    
 
-#run program
+#move played song to [History]
+def moveToHistory():
+    client = gspread.service_account(filename="cool-plasma-362004-5043b3ab9bf9.json")
+    sh = client.open("List music") # This is file's name not a sheet name
+    wkslist = sh.worksheet("test")
+    wkshistory = sh.worksheet("History")
+    wksbackup = sh.worksheet("List backup")
+
+    if needBackUp() == 2:
+        remove = wksbackup.get(f'A2:D3')
+        wkshistory.append_rows(remove)
+        wksbackup.delete_rows(2)
+        wksbackup.delete_rows(2)
+    elif needBackUp() == 1:
+        remove1 = wkslist.get(f'A2:D2')
+        wkshistory.append_rows(remove1)
+        wkslist.delete_rows(2)
+        remove2 = wksbackup.get(f'A2:D2')
+        wkshistory.append_rows(remove2)
+        wksbackup.delete_rows(2)
+    elif needBackUp() == 0:
+        remove = wkslist.get(f'A2:D3')
+        wkshistory.append_rows(remove)
+        wkslist.delete_rows(2)
+        wkslist.delete_rows(2)
+    return
+
+# run program
 runTwoSong()
+moveToHistory()
